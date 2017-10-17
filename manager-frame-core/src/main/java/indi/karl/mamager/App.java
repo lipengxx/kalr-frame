4369f38e677c8f2499f18d2f15cc09fb5a3df003
Administrator <Administrator@windows10.microdone.cn>
2017-10-17 16:20:01 +0800

    初始化代码提交

---
+++ manager-frame-core/src/main/java/indi/karl/mamager/App.java
@@ -0,0 +1,13 @@
+package indi.karl.mamager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ manager-frame-email/src/main/java/indi/karl/manager/frame/email/App.java
@@ -0,0 +1,13 @@
+package indi.karl.manager.frame.email;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
